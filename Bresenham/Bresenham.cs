aebcfe0e18d7e28e399863873d65dbf05d562cbb
goof bug <gustav.ljungberg@gmail.com>
2021-01-09 16:02:34 +0100

    Added XML comments to source code.

--- Bresenham/Bresenham.cs
+++ Bresenham/Bresenham.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// Class containing the methods of Bresenham's line algorithm.
+/// </summary>
 public class Bresenham
 {
+    /// <summary>
+    /// Calculates all the points in a line between the two supplied points using Bresenham's line algorithm.
+    /// </summary>
+    /// <param name="p0">The first point.</param>
+    /// <param name="p1">The second point.</param>
+    /// <returns>An IEnumerable that iterates over the points in the line.</returns>
+    /// <example>
+    /// <code>
+    /// var point1 = (1, 2);
+    /// var point2 = (4, 6);
+    /// foreach(var point in Bresenham.New(point1, point2)){
+    ///     Console.WriteLine(point);
+    /// }
+    /// </code>
+    /// </example>
     public static IEnumerable<ValueTuple<int, int>> New(ValueTuple<int, int> p0, ValueTuple<int, int> p1)
     {
         return New(p0.Item1, p0.Item2, p1.Item1, p1.Item2); // Just unwrap the tuples and call the method that takes separate params.
     }
 
+    /// <summary>
+    /// Calculates all the points in a line between the two supplied points using Bresenham's line algorithm.
+    /// </summary>
+    /// <param name="x0">The x coordinate of the first point.</param>
+    /// <param name="y0">The y coordinate of the first point.</param>
+    /// <param name="x1">The x coordinate of the second point.</param>
+    /// <param name="y1">The y coordinate of the second point.</param>
+    /// <returns>an IEnumerable that iterates over the points in the line.</returns>
+    /// <example>
+    /// <code>
+    /// foreach(var point in Bresenham.New((1,2), (4, 6))){
+    ///     Console.WriteLine(point);
+    /// }
+    /// </code>
+    /// </example>
     public static IEnumerable<ValueTuple<int, int>> New(int x0, int y0, int x1, int y1)
     {
         // Calculate differences (deltas) between the two points.
